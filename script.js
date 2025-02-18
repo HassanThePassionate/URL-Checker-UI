28341e0168f32f921da641861bffd5652b1ba58f
HassanThePassionate <maqsoodia777@gmail.com>
2025-02-18 11:23:47 +0500

    Update

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let question = document.querySelectorAll(".question");
-
+const question = document.querySelectorAll(".question");
+const url = document.querySelector("#url");
+const error = document.querySelector(".error-text");
+const btn = document.querySelector("#check-btn");
+const loader = document.querySelector(".loader");
+const result = document.querySelector(".result");
+const isValid = "^(https?|ftp)://[^s/$.?#].[^s]*$";
 question.forEach((question) => {
   question.addEventListener("click", (event) => {
     const active = document.querySelector(".question.active");
@@ -16,3 +21,32 @@ question.forEach((question) => {
     }
   });
 });
+const urlChecking = () => {
+  const urlInput = url.value;
+  const urlRegex = new RegExp(isValid);
+  if (urlInput === "") {
+    url.classList.remove("error-input");
+    error.style.display = "none";
+    alert("Please Enter a URL");
+    return;
+  }
+  if (urlRegex.test(urlInput)) {
+    loader.style.display = "block";
+    setTimeout(() => {
+      loader.style.display = "none";
+      result.style.display = "flex";
+    }, 2000);
+  } else {
+    url.classList.add("error-input");
+    error.style.display = "block";
+  }
+  url.addEventListener("input", () => {
+    const urlInput = url.value;
+    if (urlInput === "") {
+      url.classList.remove("error-input");
+      error.style.display = "none";
+      result.style.display = "none";
+    }
+  });
+};
+btn.addEventListener("click", urlChecking);
